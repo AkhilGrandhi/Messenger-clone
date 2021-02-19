b4846c30b62e2f9dccafeddf08fc6a225940ba52
Grandhi Akhil Sai Manoj <55430217+AkhilGrandhi@users.noreply.github.com>
2021-02-19 21:49:52 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,17 @@ import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
 
-    apiKey: "AIzaSyCznB_qBeJZof5bz3EelbEb16NnRM3UCWM",
-    authDomain: "messenger-clone-27117.firebaseapp.com",
-    databaseURL: "https://messenger-clone-27117.firebaseio.com",
-    projectId: "messenger-clone-27117",
-    storageBucket: "messenger-clone-27117.appspot.com",
-    messagingSenderId: "1042150331494",
-    appId: "1:1042150331494:web:5a428b75051d8b539c09a7",
-    measurementId: "G-WM3WCNC8K4"
+    
+    
+    
+    apiKey: "AIzaSyDIP-Em3gm0r1TnFqwJdx8XhU90gVwdPAA",
+  authDomain: "messenger-2ba95.firebaseapp.com",
+  databaseURL: "https://messenger-2ba95-default-rtdb.firebaseio.com",
+  projectId: "messenger-2ba95",
+  storageBucket: "messenger-2ba95.appspot.com",
+  messagingSenderId: "1004376542171",
+  appId: "1:1004376542171:web:8023f945ad61e24283b5fe",
+  measurementId: "G-FSMBTE6G4B"
 
 })
 
